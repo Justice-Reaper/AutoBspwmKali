2236182608985c72e59e2d77ed0384708b40dd25
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2023-12-20 19:52:09 +0100

    Update AutoBSPWM.sh

--- AutoBSPWM.sh
+++ AutoBSPWM.sh
@@ -1,3 +1,6 @@
 sudo apt update
 sudo apt -y full-upgrade -y
 [ -f /var/run/reboot-required ] && sudo reboot -f
+sudo apt install -y nvidia-detect
+sudo apt install -y nvidia-driver nvidia-cuda-toolkit
+sudo reboot -f
