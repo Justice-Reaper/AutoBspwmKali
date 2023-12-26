68b6b1e4de55ce4f196441dcea989deb707197c4
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2023-12-27 00:06:36 +0100

    Update AutoBSPWM.sh

--- AutoBSPWM.sh
+++ AutoBSPWM.sh
@@ -1,47 +1,38 @@
 #!/bin/bash
 
 # MOVEMOS EL AUTOBSPWM A DOWLOADS
-
 cd ..
 mv AutoBSPWM ~/Downloads 
 cd ~/Downloads/AutoBSPWM
 
 # ACTUALIZAMOS Y UPGRADEAMOS EL SISTEMA
-
-# Función para realizar el full-upgrade
 realizar_full_upgrade() {
     sudo apt update && sudo apt full-upgrade -y
     echo "Full-upgrade completado."
 }
 
-# Función para realizar solo la actualización
 realizar_actualizacion() {
     sudo apt update
     echo "Actualización completada."
 }
 
-# Bucle para pedir la entrada del usuario
 while true; do
-    # Solicitar confirmación al usuario
     read -p "¿Deseas realizar un full-upgrade en el sistema? (Sí/no): " respuesta
 
-    # Convertir la respuesta a minúsculas para facilitar la comparación
     respuesta=$(echo "$respuesta" | tr '[:upper:]' '[:lower:]')
 
-    # Verificar la respuesta
-    if [ "$respuesta" = "sí" ] || [ "$respuesta" = "yes" ]; then
+    if [ "$respuesta" = "sí" ]; then
         realizar_full_upgrade
         break
     elif [ "$respuesta" = "no" ]; then
         realizar_actualizacion
         break
     else
-        echo "Respuesta no válida. Por favor, responde 'Sí' o 'No'."
+        echo "Respuesta no válida. Por favor, responde 'si' o 'no'."
     fi
 done
 
 # INSTALAMOS LAS DEPENDENCIAS NECESARIAS
-
 sudo apt install imagemagick brightnessctl feh xclip bspwm sxhkd wmname polybar betterlockscreen bat lsd fzf flameshot picom rofi kitty zsh -y
 
 mkdir ~/.config/bin     
