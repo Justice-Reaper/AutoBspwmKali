d3fe516b960e0ed83cc946eaf78fa3fa7ac8bfa4
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2023-12-27 20:19:10 +0100

    Update AutoBSPWM.sh

--- AutoBSPWM.sh
+++ AutoBSPWM.sh
@@ -47,7 +47,7 @@ sudo apt install imagemagick brightnessctl feh xclip bspwm sxhkd wmname polybar 
 
 # ELIMINAMOS LOS PAQUETES QUE NO SON NECESARIOS
 echo -e "\e[32m[*]\e[0m Eliminando paquetes apt innecesarios ...\n"
-sudo apt autoremove -y
+sudo apt autoremove -y &>/dev/null
 
 # ELIMINAMOS LOS ARCHIVOS DE CACHÉ
 echo -e "\e[32m[*]\e[0m Limpiando caché de paquetes apt ...\n"
