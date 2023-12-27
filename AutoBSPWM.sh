ca815221342bef40cb002afeb0aa72ecf59e1806
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2023-12-27 16:56:31 +0100

    Update AutoBSPWM.sh

--- AutoBSPWM.sh
+++ AutoBSPWM.sh
@@ -9,7 +9,7 @@ fi
 # ACTUALIZAMOS Y UPGRADEAMOS EL SISTEMA
 while true; do
     echo -e "\e[33m[*]\e[0m ¿Deseas realizar un 'apt update' en el sistema? (SI/NO):"
-    read -p "Respuesta: " respuesta_update
+    read -p "[*]Respuesta: " respuesta_update
     respuesta_update=$(echo "$respuesta_update" | tr '[:upper:]' '[:lower:]')
 
     if [ "$respuesta_update" = "si" ] || [ "$respuesta_update" = "s" ]; then
@@ -26,7 +26,7 @@ done
 
 while true; do
     echo -e "\e[33m[*]\e[0m ¿Deseas realizar un 'full-upgrade' en el sistema? (SI/NO):"
-    read -p "Respuesta: " respuesta_upgrade
+    read -p "[*]Respuesta: " respuesta_upgrade
     respuesta_upgrade=$(echo "$respuesta_upgrade" | tr '[:upper:]' '[:lower:]')
 
     if [ "$respuesta_upgrade" = "si" ] || [ "$respuesta_upgrade" = "s" ]; then
@@ -50,21 +50,21 @@ echo -e "\e[33m[*]\e[0m Este script configurará el sistema en base al usuario p
 
 while true; do
     echo -e "\e[33m[*]\e[0m Por favor, introduce el nombre del usuario sobre el cual se aplicarán los cambios:"
-    read -p "Respuesta: " input_username
+    read -p "[*]Respuesta: " input_username
 
     if id "$input_username" &>/dev/null; then
         echo -e "\e[32m[*]\e[0m El usuario $input_username es válido.\n"
         
         while true; do
             echo -e "\e[33m[*]\e[0m ¿Es $input_username el nombre de usuario correcto? (SI/NO):"
-            read -p "Respuesta: " confirmation
+            read -p "[*]Respuesta: " confirmation
             confirmation=$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')
 
             if [ "$confirmation" = "si" ] || [ "$confirmation" = "s" ]; then
-                echo -e "\n"
+               
                 break 2
             elif [ "$confirmation" = "no" ] || [ "$confirmation" = "n" ]; then
-                echo -e "\n"
+                
                 break 
             else
                 echo -e "\e[31m[*]\e[0m Respuesta no válida. Por favor, responde 'SI' o 'NO'.\n"
