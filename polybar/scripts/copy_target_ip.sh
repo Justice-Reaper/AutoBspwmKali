5bfc843e1c96570f25556c80076cf890ab37b5c6
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2023-12-27 12:15:17 +0100

    Update copy_target_ip.sh

--- polybar/scripts/copy_target_ip.sh
+++ polybar/scripts/copy_target_ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat ~/.config/bin/target | awk '{print $1}' | tr -d '\n' | xclip -sel clip
+cat /home/user_replace/.config/bin/target | awk '{print $1}' | tr -d '\n' | xclip -sel clip
