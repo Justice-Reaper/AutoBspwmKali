81e6a4f5b9769962b88118a99c4a8237020c19f5
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2023-12-28 19:28:54 +0100

    Update increase_bright.sh

--- polybar/scripts/increase_bright.sh
+++ polybar/scripts/increase_bright.sh
@@ -2,7 +2,7 @@
 
 brillo_maximo=$(brightnessctl -q m)
 
-brillo_actual=$(cat /sys/class/backlight/amdgpu_bl0/brightness)
+brillo_actual=$(cat /sys/class/backlight/*/brightness)
 
 porcentaje_brillo=$((brillo_actual * 100 / brillo_maximo))
 
@@ -10,4 +10,4 @@ nuevo_brillo=$((porcentaje_brillo + 10))
 
 brightnessctl -q s "$nuevo_brillo%"
 
-cat /sys/class/backlight/amdgpu_bl0/actual_brightness
+cat /sys/class/backlight/*/brightness
