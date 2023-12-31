9a25faa4bb96a6f12c09960121f8dabca141ab78
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2024-01-01 00:15:51 +0100

    Update AutoBSPWM_PRUEBA.sh

--- AutoBSPWM_PRUEBA.sh
+++ AutoBSPWM_PRUEBA.sh
@@ -205,42 +205,43 @@ while true; do
     if [ "$code_editor" = "nvim" ]; then
         # INSTALANDO NVIM
         echo -e "\e[32m[*]\e[0m Instalando neovim ..."
-        apt install npm -y &>/dev/null 
+        # CREAMOS NUEVAS CONFIGURACIONES
+        execute_command apt install npm -y
         api_url="https://api.github.com/repos/neovim/neovim/releases/latest"
         download_url=$(curl -s $api_url | grep "browser_download_url.*nvim-linux64" | cut -d : -f 2,3 | tr -d '," ')
-        wget $download_url &>/dev/null 
-        tar -xf nvim-linux64.tar.gz &>/dev/null 
-        mv nvim-linux64 /opt &>/dev/null 
-        chown -R root:root /opt/nvim-linux64
+        execute_command wget $download_url 
+        execute_command  tar -xf nvim-linux64.tar.gz
+        execute_command mv nvim-linux64 /opt
+        execute_command chown -R root:root /opt/nvim-linux64
 
         # INSTALANDO NVCHAD
         echo -e "\e[32m[*]\e[0m Instalando nvchad ..."
-        mkdir /home/$input_username/.config/nvim &>/dev/null 
-        mkdir /root/.config/nvim &>/dev/null 
-        git clone https://github.com/NvChad/NvChad /home/$input_username/.config/nvim --depth 1 &>/dev/null 
-        git clone https://github.com/NvChad/NvChad /root/.config/nvim --depth 1 &>/dev/null 
+        execute_command mkdir /home/$input_username/.config/nvim
+        execute_command mkdir /root/.config/nvim
+        execute_command git clone https://github.com/NvChad/NvChad /home/$input_username/.config/nvim --depth 1
+        execute_command git clone https://github.com/NvChad/NvChad /root/.config/nvim --depth 1
 
         # CREANDO LINK SIMBÓLICO ENTRE LOS ARCHIVOS DE CONFIGURACIÓN DE NVIM DEL USUARIO ELEGIDO Y DE ROOT
         echo -e "\e[32m[*]\e[0m Creando link simbólico en los archivos de configuración de nvim ..."
-        ln -s -f /home/$input_username/.config/nvim /root/.config/nvim &>/dev/null 
+        execute_command  ln -s -f /home/$input_username/.config/nvim /root/.config/nvim
 
         # INSERTAMOS EL ALIAS DE NVIM EN LA ZSHRC
         echo -e "\e[32m[*]\e[0m Insertando alias de nvim en la zshrc ...\n"
-        sed -i "/alias icat='kitty +kitten icat'/a alias nvim='\/opt\/nvim-linux64\/bin\/nvim'" $directorio_instalacion/zshrc &>/dev/null 
-        sed -i "/alias icat='kitty +kitten icat'/a # nvim" $directorio_instalacion/zshrc &>/dev/null 
-        sed -i '/alias icat='\''kitty +kitten icat'\''/{G;}' $directorio_instalacion/zshrc &>/dev/null 
-        sed -i "/alias icat='kitty +kitten icat'/a alias nvim='\/opt\/nvim-linux64\/bin\/nvim'" $directorio_instalacion/.zshrc &>/dev/null 
-        sed -i "/alias icat='kitty +kitten icat'/a # nvim" $directorio_instalacion/.zshrc &>/dev/null 
-        sed -i '/alias icat='\''kitty +kitten icat'\''/{G;}' $directorio_instalacion/.zshrc &>/dev/null
+        execute_command sed -i "/alias icat='kitty +kitten icat'/a alias nvim='\/opt\/nvim-linux64\/bin\/nvim'" $directorio_instalacion/zshrc
+        execute_command sed -i "/alias icat='kitty +kitten icat'/a # nvim" $directorio_instalacion/zshrc
+        execute_command sed -i '/alias icat='\''kitty +kitten icat'\''/{G;}' $directorio_instalacion/zshrc
+        execute_command sed -i "/alias icat='kitty +kitten icat'/a alias nvim='\/opt\/nvim-linux64\/bin\/nvim'" $directorio_instalacion/.zshrc 
+        execute_command sed -i "/alias icat='kitty +kitten icat'/a # nvim" $directorio_instalacion/.zshrc
+        execute_command sed -i '/alias icat='\''kitty +kitten icat'\''/{G;}' $directorio_instalacion/.zshrc
 
         # INSERTAMOS EN EL SXHKDRC LOS SHORTCUS DE NVIM
         echo -e "\e[32m[*]\e[0m Insertando shortcuts de nvim en el sxhkdrc ...\n"
-        echo -e '\n# nvim\nsuper + shift + e\n\tnvim' >> $directorio_instalacion/sxhkd/sxhkdrc   
+        execute_command echo -e '\n# nvim\nsuper + shift + e\n\tnvim' >> $directorio_instalacion/sxhkd/sxhkdrc   
         break
     elif [ "$code_editor" = "vscode" ]; then
         echo -e "\e[32m[*]\e[0m Instalando vscode ...\n"
-        wget  https://vscode.download.prss.microsoft.com/dbazure/download/stable/0ee08df0cf4527e40edc9aa28f4b5bd38bbff2b2/code_1.85.1-1702462158_amd64.deb &>/dev/null 
-        apt install ./code_1.85.1-1702462158_amd64.deb &>/dev/null 
+        execute_command wget  https://vscode.download.prss.microsoft.com/dbazure/download/stable/0ee08df0cf4527e40edc9aa28f4b5bd38bbff2b2/code_1.85.1-1702462158_amd64.deb
+        execute_command apt install ./code_1.85.1-1702462158_amd64.deb
         echo -e '\n# vscode\nsuper + shift + e\n\tcode' >> $directorio_instalacion/sxhkd/sxhkdrc   
         break
     else
@@ -250,13 +251,13 @@ done
 
 # SUSTITUIMOS USER_REPLACE POR EL USUARIO ELEGIDO
 echo -e "\e[32m[*]\e[0m Configurando ficheros ...\n"
-sed -i "s/user_replace/$input_username/g" $directorio_instalacion/polybar/* &>/dev/null 
-sed -i "s/user_replace/$input_username/g" $directorio_instalacion/polybar/scripts/* &>/dev/null 
-sed -i "s/user_replace/$input_username/g" $directorio_instalacion/bspwm/* &>/dev/null 
-sed -i "s/user_replace/$input_username/g" $directorio_instalacion/bspwm/scripts* &>/dev/null 
-sed -i "s/user_replace/$input_username/g" $directorio_instalacion/sxhkd/* &>/dev/null 
-sed -i "s/user_replace/$input_username/g" $directorio_instalacion/p10k.zsh &>/dev/null 
-sed -i "s/user_replace/$input_username/g" $directorio_instalacion/zshrc &>/dev/null 
+execute_command sed -i "s/user_replace/$input_username/g" $directorio_instalacion/polybar/*
+execute_command sed -i "s/user_replace/$input_username/g" $directorio_instalacion/polybar/scripts/*
+execute_command sed -i "s/user_replace/$input_username/g" $directorio_instalacion/bspwm/*
+execute_command sed -i "s/user_replace/$input_username/g" $directorio_instalacion/bspwm/scripts*
+execute_command sed -i "s/user_replace/$input_username/g" $directorio_instalacion/sxhkd/*
+execute_command sed -i "s/user_replace/$input_username/g" $directorio_instalacion/p10k.zsh
+execute_command sed -i "s/user_replace/$input_username/g" $directorio_instalacion/zshrc
 
 # OBTENEMOS LAS INTERFACES DE RED
 interfaces=$(ip -o link show | awk -F': ' '{print $2}')
@@ -278,14 +279,14 @@ for interface in $interfaces; do
 done
 
 # SUSTITUIMOS LAS INTERFACES DE RED EN LOS SCRIPTS DE LA POLYBAR
-sed -i "s/ethernet_replace/$ethernet_interface/g" $directorio_instalacion/polybar/scripts/* &>/dev/null 
-sed -i "s/wifi_replace/$wifi_interface/g" $directorio_instalacion/polybar/scripts/* &>/dev/null 
+execute_command sed -i "s/ethernet_replace/$ethernet_interface/g" $directorio_instalacion/polybar/scripts/*
+execute_command sed -i "s/wifi_replace/$wifi_interface/g" $directorio_instalacion/polybar/scripts/*
 
 # SUSTITUIMOS LA BATERÍA Y EL ADAPTADOR
 battery="$(ls -1 /sys/class/power_supply/ | cut -d'/' -f8- | tail -n 1)"
 adapter="$(ls -1 /sys/class/power_supply/ | cut -d'/' -f8- | head -n 1)"
-sed -i "s/battery_replace/$battery/g" "$directorio_instalacion/polybar/config.ini" &>/dev/null 
-sed -i "s/adapter_replace/$adapter/g" "$directorio_instalacion/polybar/config.ini" &>/dev/null 
+execute_command sed -i "s/battery_replace/$battery/g" "$directorio_instalacion/polybar/config.ini"
+execute_command sed -i "s/adapter_replace/$adapter/g" "$directorio_instalacion/polybar/config.ini"
 
 # CONFIGURANDO FONTS
 echo -e "\e[32m[*]\e[0m Configurando fonts ...\n"
@@ -297,7 +298,7 @@ cp -r Wallpapers /home/$input_username
 
 # CONFIGURANDO BETTERLOCKSCREEN
 echo -e "\e[32m[*]\e[0m Configurando betterlockscreen ...\n"
-betterlockscreen -u /home/$input_username/Wallpapers &>/dev/null 
+execute_command betterlockscreen -u /home/$input_username/Wallpapers
 
 # CONFIGURANDO SXHKD
 echo -e "\e[32m[*]\e[0m Configurando sxhkd ...\n"
@@ -310,7 +311,7 @@ cp -r kitty /root/.config
 
 # CREAMOS UN LINK SIMBÓLICO ENTRE LOS ARCHIVOS DE CONFIGURACIÓN DE LA KITTY DEL USUARIO ELEGIDO Y LOS DE ROOT
 echo -e "\e[32m[*]\e[0m Creando link simbólico en kitty.conf y kitty.color ...\n"
-ln -s -f /home/$input_username/.config/kitty /root/.config/kitty
+execute_command ln -s -f /home/$input_username/.config/kitty /root/.config/kitty
 
 # CONFIGURANDO PICOM
 echo -e "\e[32m[*]\e[0m Configurando picom ...\n"
@@ -344,35 +345,35 @@ echo -e "\e[32m[*]\e[0m Configurando polybar ...\n"
 cp -r polybar /home/$input_username/.config
 cd /home/$input_username/.config/polybar/scripts 
 chmod +x *
-mkdir /home/$input_username/.config/bin &>/dev/null 
-touch /home/$input_username/.config/bin/target &>/dev/null 
+execute_command mkdir /home/$input_username/.config/bin
+execute_command touch /home/$input_username/.config/bin/target
 cd "$directorio_instalacion"
 
 # CONFIGURANDO POWERLEVEL10K
 echo -e "\e[32m[*]\e[0m Configurando powerlevel10k del usuario $input_username ...\n"
-rm -rf /home/$input_username/powerlevel10k &>/dev/null 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$input_username/powerlevel10k &>/dev/null 
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>/home/$input_username/.zshrc &>/dev/null 
-mv zshrc .zshrc &>/dev/null 
-mv p10k.zsh .p10k.zsh &>/dev/null 
+execute_command rm -rf /home/$input_username/powerlevel10k
+execute_command git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$input_username/powerlevel10k
+execute_command echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>/home/$input_username/.zshrc
+execute_command mv zshrc .zshrc
+execute_command mv p10k.zsh .p10k.zsh
 cp .p10k.zsh /home/$input_username
 cp .zshrc /home/$input_username
 
 # CONFIGURANDO POWERLEVEL10K DE ROOT
 echo -e "\e[32m[*]\e[0m Configurando powerlevel10k del usuario root ...\n"
-rm -rf /root/powerlevel10k &>/dev/null 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k &>/dev/null 
-sh -c "echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> /root/.zshrc" &>/dev/null 
+execute_command rm -rf /root/powerlevel10k 
+execute_command git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k 
+execute_command sh -c "echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> /root/.zshrc" 
 cp .p10k.zsh /root 
 cp .zshrc /root
 
 # CREAMOS UN LINK SIMBÓLICO ENTRE LA ZSHRC DEL USUARIO ELEGIDO Y LA ZSHRC DE ROOT
 echo -e "\e[32m[*]\e[0m Creando link simbólico en la zshrc ...\n"
-ln -s -f /home/$input_username/.zshrc /root/.zshrc
+execute_command ln -s -f /home/$input_username/.zshrc /root/.zshrc
 
 # CREAMOS UN LINK SIMBÓLICO ENTRE LA P10K DEL USUARIO ELEGIDO Y EL P10K DE ROOT
 echo -e "\e[32m[*]\e[0m Creando link simbólico en el archivo p10k.zsh ...\n"
-ln -s -f /home/$input_username/.p10k.zsh /root/.p10k.zsh
+execute_command ln -s -f /home/$input_username/.p10k.zsh /root/.p10k.zsh
 
 # LE ASIGNAMOS EL PROPIETARIO CORRECTO A LOS ARCHIVOS
 echo -e "\e[32m[*]\e[0m Asignando el propietario correcto a los archivos de configuración ...\n"
@@ -380,7 +381,7 @@ chown -R $input_username:$input_username /home/$input_username
 
 # ELIMINAMOS LOS PAQUETES QUE NO SON NECESARIOS
 echo -e "\e[32m[*]\e[0m Eliminando paquetes apt innecesarios ...\n"
-apt autoremove -y &>/dev/null 
+execute_command apt autoremove -y
 
 # ELIMINAMOS LOS ARCHIVOS DE CACHÉ
 echo -e "\e[32m[*]\e[0m Limpiando caché de paquetes apt ...\n"
