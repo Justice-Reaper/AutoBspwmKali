e2adcded06b1ce931e91909a72744a628767a8d6
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2023-12-31 13:52:29 +0100

    Update copy_ethernet_ip.sh

--- polybar/scripts/copy_ethernet_ip.sh
+++ polybar/scripts/copy_ethernet_ip.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-IP_ETH0=$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}' | tr -d '\n')
+IP_ETHERNET=$(/usr/sbin/ifconfig ethernet_replace | grep "inet " | awk '{print $2}' | tr -d '\n')
 
-if [ -n "$IP_ETH0" ]; then
-    echo -n "$IP_ETH0" | xclip -sel clip
+if [ -n "$IP_ETHERNET" ]; then
+    echo -n "$IP_ETHERNET" | xclip -sel clip
 fi
 
-IP_WLAN0=$(/usr/sbin/ifconfig wlan0 | grep "inet " | awk '{print $2}' | tr -d '\n')
+IP_WIFI=$(/usr/sbin/ifconfig wifi_replace | grep "inet " | awk '{print $2}' | tr -d '\n')
 
-if [ -n "$IP_WLAN0" ]; then
-    echo -n "$IP_WLAN0" | xclip -sel clip
+if [ -n "$IP_WIFI" ]; then
+    echo -n "$IP_WIFI" | xclip -sel clip
 fi
