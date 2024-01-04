bd9813927a16064eab8683cf368c55b385472bef
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2024-01-04 22:32:52 +0100

    Update ethernet_status.sh

--- polybar/scripts/ethernet_status.sh
+++ polybar/scripts/ethernet_status.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-IFACE_ETHERNET=$(ip addr show ethernet_replace | grep "state UP" -A2 | awk '/inet / {print $2}' | cut -d'/' -f1)
-IFACE_WIFI=$(ip addr show wifi_replace | grep "state UP" -A2 | awk '/inet / {print $2}' | cut -d'/' -f1)
+IFACE_ETHERNET=$(ip addr show ethernet_replace | awk '/inet / {print $2}' | cut -d'/' -f1)
+IFACE_WIFI=$(ip addr show wifi_replace | awk '/inet / {print $2}' | cut -d'/' -f1)
 
 if [ -n "$IFACE_ETHERNET" ]; then
     IP_ADDRESS="$IFACE_ETHERNET"
-    ICON="%{F#70A5EB}󰈀"  # Icono para eth0
+    ICON="%{F#70A5EB}󰈀" 
 elif [ -n "$IFACE_WIFI" ]; then
     IP_ADDRESS="$IFACE_WIFI"
-    ICON="%{F#70A5EB}󰤢"  # Icono para wlan0
+    ICON="%{F#70A5EB}󰤢"
 else
     IP_ADDRESS="Disconnected"
-    ICON="%{F#70A5EB}󱘖"  # Icono predeterminado
+    ICON="%{F#70A5EB}󱘖"
 fi
 
 echo "$ICON %{F#DEE1E6}$IP_ADDRESS%{u-}"
