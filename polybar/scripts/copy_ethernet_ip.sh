78194c664a6da8f8324d54d4d234da72697b9122
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2024-01-05 22:51:38 +0100

    Update copy_ethernet_ip.sh

--- polybar/scripts/copy_ethernet_ip.sh
+++ polybar/scripts/copy_ethernet_ip.sh
@@ -1,13 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-IP_WIFI=$(/usr/sbin/ifconfig wifi_replace | grep "inet " | awk '{print $2}' | tr -d '\n')
+interfaces=$(ip -o link show | awk -F': ' '{print $2}')
 
-if [ -n "$IP_WIFI" ]; then
-    echo -n "$IP_WIFI" | xclip -sel clip
+wifi_interface=""
+ethernet_interface=""
+
+for interface in $interfaces; do
+    if echo "$interface" | grep -q "wl"; then
+        wifi_interface=$interface
+        icon="%{F#70A5EB}󰤢"
+
+    elif echo "$interface" | grep -q "en\|eth"; then
+        ethernet_interface=$interface
+        icon="%{F#70A5EB}󰈀"
+    fi
+done
+
+ip_address=""
+icon=""
+
+if [ -n "$ethernet_interface" ]; then
+    ip_address=$(ip addr show $ethernet_interface | awk '/inet / {print $2}' | cut -d'/' -f1)
+
+elif [ -n "$wifi_interface" ]; then
+    ip_address=$(ip addr show $wifi_interface | awk '/inet / {print $2}' | cut -d'/' -f1)
 fi
 
-IP_ETHERNET=$(/usr/sbin/ifconfig ethernet_replace | grep "inet " | awk '{print $2}' | tr -d '\n')
-
-if [ -n "$IP_ETHERNET" ]; then
-    echo -n "$IP_ETHERNET" | xclip -sel clip
-fi
+echo -n "$ip_address" | xclip -sel clip
