b86794c28f2a998594d0ea110599a9c966be0072
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2024-01-05 21:27:18 +0100

    Update ethernet_status.sh

--- polybar/scripts/ethernet_status.sh
+++ polybar/scripts/ethernet_status.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-IFACE_ETHERNET=$(ip addr show ethernet_replace | awk '/inet / {print $2}' | cut -d'/' -f1)
-IFACE_WIFI=$(ip addr show wifi_replace | awk '/inet / {print $2}' | cut -d'/' -f1)
+interfaces=$(ifconfig -a | grep '^[a-zA-Z]' | awk '{print $1}' | sed 's/://')
 
-if [ -n "$IFACE_ETHERNET" ]; then
-    IP_ADDRESS="$IFACE_ETHERNET"
-    ICON="%{F#70A5EB}󰈀" 
-elif [ -n "$IFACE_WIFI" ]; then
-    IP_ADDRESS="$IFACE_WIFI"
-    ICON="%{F#70A5EB}󰤢"
-else
-    IP_ADDRESS="Disconnected"
-    ICON="%{F#70A5EB}󱘖"
-fi
+ip_address=""
+icon=""
 
-echo "$ICON %{F#DEE1E6}$IP_ADDRESS%{u-}"
+for interface in $interfaces; do
+    if echo "$interface" | grep -q "wl"; then
+        ip_address=$(ip addr show $interface | awk '/inet / {print $2}' | cut -d'/' -f1)
+        icon="%{F#70A5EB}󰈀"
+    elif echo "$interface" | grep -q "en\|eth"; then
+        ip_address=$(ip addr show $interface | awk '/inet / {print $2}' | cut -d'/' -f1)
+        icon="%{F#70A5EB}󰤢"
+    else
+        ip_address="Disconnected"
+        icon="%{F#70A5EB}󱘖"
+    fi
+done
+
+echo "$icon %{F#DEE1E6}$ip_address%{u-}"
