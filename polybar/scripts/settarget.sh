d58f6751faeaff91a35733aba3b7f93c171d9fb9
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2024-01-07 23:59:26 +0100

    Update settarget.sh

--- polybar/scripts/settarget.sh
+++ polybar/scripts/settarget.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 ip_address=$(cat /home/user_replace/.config/bin/target | awk '{print $1}')
-machine_name=$(cat /home/user_replace/.config/bin/target | awk '{print $2}')
 
-if [ $ip_address ] && [ $machine_name ]; then
-    echo "%{F#E05F65}󰓾 %{F#DEE1E6}$ip_address%{u-} - $machine_name"
+if [ $ip_address ]; then
+  echo "%{F#E05F65}󰓾 %{F#DEE1E6}$ip_address%{u-}"
+
 else
-    if [ $ip_address ]; then
-	echo "%{F#E05F65}󰓾 %{F#DEE1E6}$ip_address%{u-}"
-    else
-        echo "%{F#E05F65}󰓾 %{u-}%{F#DEE1E6}No target"
-    fi
+  echo "%{F#E05F65}󰓾 %{u-}%{F#DEE1E6}No target"
 fi
