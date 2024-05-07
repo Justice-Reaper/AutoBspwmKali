446033320cb2e3cb15c15b6366d6fa20caf49df7
Justice-Reaper <121357402+Justice-Reaper@users.noreply.github.com>
2024-05-08 00:14:52 +0200

    Update ethernet_status.sh

--- polybar/scripts/ethernet_status.sh
+++ polybar/scripts/ethernet_status.sh
@@ -3,7 +3,7 @@
 interfaces=$(ip -o link show | awk -F': ' '{print $2}')
 
 wifi_interface=$(ip -o link show | awk -F': ' '{print $2}' | grep "wl")
-ethernet_interface=$(ip -o link show | awk -F': ' '{print $2}' | grep "en\|eth")
+ethernet_interface=$(ip -o link show | awk -F': ' '{print $2}' | grep -E "^en|^eth")
 
 ip_address_wifi=$(ip addr show $wifi_interface | awk '/inet / {print $2}' | cut -d'/' -f1)
 if [ -n "$ip_address_wifi" ]; then
