10f5ea2b6ae04aa380d5edf59a4e2ce61bd6e355
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2024-12-12 20:28:02 +0100

    Update settarget.sh

--- polybar/scripts/settarget.sh
+++ polybar/scripts/settarget.sh
@@ -3,8 +3,8 @@
 ip_address=$(cat /home/user_replace/.config/bin/target | awk '{print $1}')
 
 if [ $ip_address ]; then
-    echo "%{F#E05F65}󰓾 %{F#DEE1E6}$ip_address%{u-}"
+    echo "%{F#E05F65}󰓾 %{F#DEE1E6}$ip_address"
 
 else
-    echo "%{F#E05F65}󰓾 %{u-}%{F#DEE1E6}No target"
+    echo "%{F#E05F65}󰓾 %{F#DEE1E6}No target"
 fi
