ccf2a2c08f8f6ac3aeef922be1d13b0004864e36
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2024-12-12 19:50:53 +0100

    Update copy_ethernet_ip.sh

--- polybar/scripts/copy_ethernet_ip.sh
+++ polybar/scripts/copy_ethernet_ip.sh
@@ -7,13 +7,11 @@ ethernet_interface=$(ip -o link show | awk -F': ' '{print $2}' | grep "en\|eth")
 
 ip_address_wifi=$(ip addr show $wifi_interface | awk '/inet / {print $2}' | cut -d'/' -f1)
 if [ -n "$ip_address_wifi" ]; then
-    icon="%{F#70A5EB}󰤢"
     ip_address=$ip_address_wifi
 fi
 
 ip_address_ethernet=$(ip addr show $ethernet_interface | awk '/inet / {print $2}' | cut -d'/' -f1)
 if [ -n "$ip_address_ethernet" ]; then
-    icon="%{F#70A5EB}󰈀"
     ip_address=$ip_address_ethernet
 fi
 
