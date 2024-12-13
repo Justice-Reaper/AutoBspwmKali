5db47e91ebf392d1601c0d32e244b63a99c32560
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2024-12-13 19:19:46 +0100

    Update AutoBSPWM.sh

--- AutoBSPWM.sh
+++ AutoBSPWM.sh
@@ -210,9 +210,8 @@ instalacion_drivers_nvidia(){
 
 configuracion_tecla_fn(){
     while true; do
-        echo -e "\e[33m[*]\e[0m Aparecerá un output así (keysym 0x1008ff13, XF86AudioRaiseVolume) cuando introduzcas una combinación de teclas. Debes introducir el tercer parámetro, en este caso es XF86AudioRaiseVolume"
-        read -p "$(echo -e "\e[33m[*]\e[0m Pon el puntero del ratón encima de la ventana blanca e introduce el parámetro que te aprece en la consola $1: ")" response
         kitty --detach bash -c "xev | grep 'keysym'; exec bash"
+        read -p "$(echo -e "\e[33m[*]\e[0m Aparecerá un output así (keysym 0x1008ff13, XF86AudioRaiseVolume) cuando introduzcas una combinación de teclas. Debes introducir el tercer parámetro, en este caso es XF86AudioRaiseVolume\nPon el puntero del ratón encima de la ventana blanca e introduce el parámetro que te aprece en la consola $1: ")" response
         echo "$response" >> /tmp/keys
         break
     done
