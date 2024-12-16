81d76a91b880f882d08f941559e05ca83e0a7ad1
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2024-12-16 17:13:47 +0100

    Update usb_add_sound.sh

--- sound/scripts/usb_add_sound.sh
+++ sound/scripts/usb_add_sound.sh
@@ -3,4 +3,4 @@
 export XDG_RUNTIME_DIR="/run/user/1000"
 export PULSE_SERVER="unix:/run/user/1000/pulse/native"
 
-/usr/bin/paplay /home/justice-reaper/.config/sound/wavs/add-usb.wav &
+/usr/bin/paplay /home/user_replace/.config/sound/wavs/add-usb.wav &
