f77b3bedd6d98bd685074b9a101a5b09ea0163b6
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2025-04-26 00:35:14 +0200

    Update increase_brightness.sh

--- polybar/scripts/increase_brightness.sh
+++ polybar/scripts/increase_brightness.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-brillo_maximo=$(brightnessctl -q m)
-brillo_actual=$(cat /sys/class/backlight/*/brightness)
-porcentaje_brillo=$((brillo_actual * 100 / brillo_maximo))
-nuevo_brillo=$((porcentaje_brillo + 10))
+max_brightness=$(brightnessctl -q m)
+current_brightness=$(cat /sys/class/backlight/*/brightness)
+brightness_percentage=$((current_brightness * 100 / max_brightness))
+new_brightness=$((brightness_percentage + 10))
 
-brightnessctl -q s "$nuevo_brillo%"
+brightnessctl -q s "$new_brightness%"
 
 cat /sys/class/backlight/*/brightness
