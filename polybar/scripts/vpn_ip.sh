e49b5b9d8d81ae4b46b9ce05a97e0a516627aaa7
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2025-04-26 00:38:19 +0200

    Update vpn_ip.sh

--- polybar/scripts/vpn_ip.sh
+++ polybar/scripts/vpn_ip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
+interface=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
 
-if [ "$IFACE" = "tun0" ]; then
+if [ "$interface" = "tun0" ]; then
     echo "%{F#76B97F}󰞀 %{F#DEE1E6}$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')"
 else
     echo "%{F#76B97F}󰞀 %{F#DEE1E6}Disconnected"
