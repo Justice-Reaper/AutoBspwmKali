eef048732b9afa897788e42495dee282d4bddc9a
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2025-04-26 00:36:52 +0200

    Update increase_volume.sh

--- polybar/scripts/increase_volume.sh
+++ polybar/scripts/increase_volume.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VOLUME=$(pactl list sinks | grep 'Volume:' | awk '{print $5; exit}' | sed 's/%//')
+volume=$(pactl list sinks | grep 'Volume:' | awk '{print $5; exit}' | sed 's/%//')
 
-if [ "$VOLUME" -lt "100" ]; then
+if [ "$volume" -lt "100" ]; then
     pactl -- set-sink-volume 0 +10%
 fi
 
