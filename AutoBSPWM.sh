5a20cf654a147f68d6d04f154d2403e19d5d4faa
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2025-07-03 17:56:37 +0200

    Update AutoBSPWM.sh

--- AutoBSPWM.sh
+++ AutoBSPWM.sh
@@ -400,14 +400,12 @@ burpsuite_professional_installation(){
     apt install git axel openjdk-21-jre -y
     rm -rf /opt/*Burpsuite-Professional*  
     cd /opt
-
     git clone https://github.com/xiv3r/Burpsuite-Professional.git 
     cd Burpsuite-Professional
-
     version=$(curl -s https://portswigger.net/burp/releases/community/latest -L | grep -oP 'version=\K[0-9]+\.[0-9]+\.[0-9]+' | head -1)
     url="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar"
     axel "$url" -o "burpsuite_pro_v$version.jar"
-
+    (java -jar loader.jar) &
     echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
     chmod +x burpsuitepro
     cp burpsuitepro /bin/burpsuitepro
