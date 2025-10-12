d9cf364a5b58d9d45c443df5529b2413b51649ee
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2025-10-12 17:12:29 +0200

    Update battery_notification.sh

--- dunst/scripts/battery_notification.sh
+++ dunst/scripts/battery_notification.sh
@@ -12,11 +12,11 @@ battery_warning_file=/home/user_replace/.config/bin/battery_warning
 battery_fully_charged_file=/home/user_replace/.config/bin/battery_fully_charged
 
 if [ "$battery_level" -le "$warning_level" ] && [ "$(cat $battery_warning_file)" != "True" ] && [ "$battery_charging" -eq 0 ]; then
-    notify-send "Low Battery" "${battery_level}% of battery remaining" -u critical -i "battery-alert" -r 9991
     echo 'True' > "$battery_warning_file"
     echo 'False' > "$battery_charging_file"
     echo 'False' > "$battery_discharging_file"
     echo 'False' > "$battery_fully_charged_file"
+    notify-send "Low Battery" "${battery_level}% of battery remaining" -c critical-battery -i "/home/user_replace/.config/dunst/icons/battery-alert.png" -r 9991
 
 elif [ "$battery_discharging" -eq 1 ] && [ "$(cat $battery_discharging_file)" != "True" ] && [ "$battery_level" -gt 10 ] && [ "$battery_level" -le 99 ]; then
     echo 'True' > "$battery_discharging_file"
@@ -25,16 +25,16 @@ elif [ "$battery_discharging" -eq 1 ] && [ "$(cat $battery_discharging_file)" !=
     echo 'False' > "$battery_charging_file"
 
 elif [ "$battery_charging" -eq 1 ] && [ "$(cat $battery_charging_file)" != "True" ] && [ "$battery_level" -le 99 ]; then
-    notify-send "Charging" "${battery_level}% of battery charged" -u normal -i "battery-charging" -r 9991
     echo 'True' > "$battery_charging_file"
     echo 'False' > "$battery_warning_file"
     echo 'False' > "$battery_fully_charged_file"
     echo 'False' > "$battery_discharging_file"
+    notify-send "Charging" "${battery_level}% of battery charged" -c normal-battery -i "/home/user_replace/.config/dunst/icons/battery-charging.png" -r 9991
 
 elif [ "$battery_level" -eq 100 ] && [ "$(cat $battery_fully_charged_file)" != "True" ]; then
     echo 'True' > "$battery_fully_charged_file"
     echo 'False' > "$battery_charging_file"
     echo 'False' > "$battery_warning_file"
     echo 'False' > "$battery_discharging_file"
-    notify-send "Battery Charged" "Battery is fully charged" -u normal -i "battery-fully-charged" -r 9991
+    notify-send "Battery Charged" "Battery is fully charged" -c normal-battery -i "/home/user_replace/.config/dunst/icons/battery-fully-charged.png" -r 9991
 fi
