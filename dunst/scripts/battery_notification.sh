2b64084b466a09896296039cdb41407e37508650
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2025-10-13 01:05:37 +0200

    Update battery_notification.sh

--- dunst/scripts/battery_notification.sh
+++ dunst/scripts/battery_notification.sh
@@ -16,7 +16,7 @@ if [ "$battery_level" -le "$warning_level" ] && [ "$(cat $battery_warning_file)"
     echo 'False' > "$battery_charging_file"
     echo 'False' > "$battery_discharging_file"
     echo 'False' > "$battery_fully_charged_file"
-    notify-send "Low Battery" "${battery_level}% of battery remaining" -c battery-critical -i "/home/user_replace/.config/dunst/icons/battery-warning.png" -r 9991
+    notify-send "Low Battery" "${battery_level}% of battery remaining" -u critical -c battery-critical -i "/home/user_replace/.config/dunst/icons/battery-warning.png" -r 9991
 
 elif [ "$battery_discharging" -eq 1 ] && [ "$(cat $battery_discharging_file)" != "True" ] && [ "$battery_level" -gt 10 ] && [ "$battery_level" -le 99 ]; then
     echo 'True' > "$battery_discharging_file"
@@ -29,12 +29,12 @@ elif [ "$battery_charging" -eq 1 ] && [ "$(cat $battery_charging_file)" != "True
     echo 'False' > "$battery_warning_file"
     echo 'False' > "$battery_fully_charged_file"
     echo 'False' > "$battery_discharging_file"
-    notify-send "Charging" "${battery_level}% of battery charged" -c battery-normal -i "/home/user_replace/.config/dunst/icons/battery-charging.png" -r 9991
+    notify-send "Charging" "${battery_level}% of battery charged" -u normal -c battery-normal -i "/home/user_replace/.config/dunst/icons/battery-charging.png" -r 9991
 
 elif [ "$battery_level" -eq 100 ] && [ "$(cat $battery_fully_charged_file)" != "True" ]; then
     echo 'True' > "$battery_fully_charged_file"
     echo 'False' > "$battery_charging_file"
     echo 'False' > "$battery_warning_file"
     echo 'False' > "$battery_discharging_file"
-    notify-send "Battery Charged" "Battery is fully charged" -c battery-normal -i "/home/user_replace/.config/dunst/icons/battery-fully-charged.png" -r 9991
+    notify-send "Battery Charged" "Battery is fully charged" -u normal -c battery-normal -i "/home/user_replace/.config/dunst/icons/battery-fully-charged.png" -r 9991
 fi
