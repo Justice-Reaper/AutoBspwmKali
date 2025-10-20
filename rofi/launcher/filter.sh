d0b6b091c77f80ba3a23433c192a3839f183db44
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2025-10-21 00:58:39 +0200

    Update filter.sh

--- rofi/launcher/filter.sh
+++ rofi/launcher/filter.sh
@@ -12,35 +12,6 @@ find /home/justice-reaper/.local/share/applications -name "*.desktop" 2>/dev/nul
         if grep -qi "^Name=.*\(picom\|terminal\|rofi\)" "$file"; then
             continue
         fi
-        
-        if grep -q "^Exec=" "$file"; then
-            exec_line=$(grep "^Exec=" "$file" | head -1)
-            original_command=$(echo "$exec_line" | sed 's/^Exec=//')
-
-            new_command=""
-            changed=false
-
-            while read -r word; do
-                if [ -n "$word" ]; then
-                    if [[ "$word" != /* ]] && [[ "$word" != *%* ]]; then
-                        absolute_path=$(which "$word" 2>/dev/null)
-                        if [ $? -eq 0 ] && [ -n "$absolute_path" ]; then
-                            new_command="$new_command $absolute_path"
-                            changed=true
-                        else
-                            new_command="$new_command $word"
-                        fi
-                    else
-                        new_command="$new_command $word"
-                    fi
-                fi
-            done < <(echo "$original_command" | tr ' ' '\n')
-
-            if [ "$changed" = true ]; then
-                new_command=$(echo "$new_command" | sed 's/^ //')
-                sed -i "s|^$exec_line|Exec=$new_command|" "$file"
-            fi
-        fi
 
         if grep -qi "^Categories=" "$file"; then
             if ! grep -qi "Rofi;" "$file"; then
